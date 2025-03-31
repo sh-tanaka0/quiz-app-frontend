cf7c600fef29221c800663c2c5768e7aba62fb24
sh-tanaka0 <123asde.med@gmail.com>
2025-03-31 13:58:29 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // プロジェクトルートのHTMLファイル
+    "./src/**/*.{js,ts,jsx,tsx}", // srcディレクトリ以下の全てのJS/TS/JSX/TSXファイル
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
