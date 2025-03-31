53ceee037446cb4ed57e0f46b836f563a5df8ad5
sh-tanaka0 <123asde.med@gmail.com>
2025-03-31 14:46:45 +0900

    必要なライブラリを追加、デモ画面作成

--- src/App.tsx
+++ src/App.tsx
@@ -9,57 +9,12 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
-
-// サンプルデータ
-const data = [
-  { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
-  { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
-  { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
-  { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
-  { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
-  { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
-  { name: "Page G", uv: 3490, pv: 4300, amt: 2100 },
-];
+import AnswerInputScreen from "./AnswerInputScreen";
 
 function App() {
   return (
-    <div className="container mx-auto p-4">
-      {/* Tailwind CSS を使ったタイトル */}
-      <h1 className="text-3xl font-bold text-blue-600 mb-6">
-        React + Recharts + Tailwind CSS
-      </h1>
-
-      <p className="text-gray-700 mb-4">
-        これはTailwind CSSでスタイリングされたテキストです。
-      </p>
-
-      {/* Recharts を使ったグラフ */}
-      <div style={{ width: "100%", height: 300 }}>
-        <ResponsiveContainer>
-          <LineChart
-            data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line
-              type="monotone"
-              dataKey="pv"
-              stroke="#8884d8"
-              activeDot={{ r: 8 }}
-            />
-            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-          </LineChart>
-        </ResponsiveContainer>
-      </div>
+    <div>
+      <AnswerInputScreen></AnswerInputScreen>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // この設定があるか確認
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
